c398e94853293291aa2dc7e3d592db1424f6255e
harxhmstr <harsh.cllg@gmail.com>
2026-08-19 23:58:39 +0530

    stream api

--- src/prac/Main.java
+++ src/prac/Main.java
@@ -1,5 +1,7 @@
 package prac;
 
+import java.util.*;
+
 public class Main{
 	public static void main(String[] args) {
 		TaskValidator task = (t)->{
@@ -10,8 +12,14 @@ public class Main{
 		Task t1 = new Task();
 		Task t2 = new Task("Work","High");
 		Task t3 = new Task(null , "low");
-		System.out.println(task.isValid(t1));
-		System.out.println(task.isValid(t3));
-		System.out.println(task.isValid(t2));	
+		List<Task> tasksList = new ArrayList<>();
+		tasksList.add(t1);
+		tasksList.add(t2);
+		tasksList.add(t3);
+		tasksList.add(new Task("impo work",null));
+		tasksList.add(new Task("okish work","medium"));
+		List<Task> result = tasksList.stream().filter(task::isValid).toList();
+		System.out.println(result.size());
+		result.forEach(t->System.out.println(t.getTitle()));
 	}
 }
